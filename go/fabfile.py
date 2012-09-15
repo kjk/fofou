b1b053fd69e9130496c1de561c42eb15ff42adf1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-14 18:09:47 -0700

    add code to delete old deploys; remove commented-out code

--- go/fabfile.py
+++ go/fabfile.py
@@ -8,6 +8,8 @@ from fabric.contrib import *
 env.hosts = ['test.fofou.org']
 env.user = 'fofou'
 
+app_dir = 'www/app'
+
 def git_ensure_clean():
 	out = subprocess.check_output(["git", "status", "--porcelain"])
 	if len(out) != 0:
@@ -19,7 +21,6 @@ def git_pull():
 	local("git pull")
 
 def git_trunk_sha1():
-	# TODO: use "git rev-parse origin" instead?
 	return subprocess.check_output(["git", "log", "-1", "--pretty=format:%H"])
 
 def delete_file(p):
@@ -29,16 +30,10 @@ def delete_file(p):
 def ensure_remote_dir_exists(p):
 	if not files.exists(p):
 		abort("dir '%s' doesn't exist on remote server" % p)
-	#with settings(warn_only=True):
-	#	if run("test -d %s" % p).failed:
-	#		abort("dir '%s' doesn't exist on remote server" % p)
 
 def ensure_remote_file_exists(p):
 	if not files.exists(p):
 		abort("dir '%s' doesn't exist on remote server" % p)
-	#with settings(warn_only=True):
-	#	if run("test -f %s" % p).failed:
-	#		abort("file '%s' doesn't exist on remote server" % p	)
 
 def add_dir_files(zip_file, dir):
 	for (path, dirs, files) in os.walk(dir):
@@ -58,44 +53,69 @@ def zip_files(zip_path):
 	add_dir_files(zf, "static")
 	zf.close()
 
+def delete_old_deploys(to_keep=5):
+	with cd(app_dir):
+		out = run('ls -1trF')
+		lines = out.split("\n")
+		i = 0
+		dirs_to_del = []
+		while i < len(lines):
+			s = lines[i].strip()
+			# extra precaution: skip dirs right after "prev@", "current@", they
+			# are presumed to be their symlink targets
+			if s in ["prev@", "current@"]:
+				i += 1
+				to_keep -= 1
+			else:
+				if len(s) == 41: # s == "0111cb7bdd014850e8c11ee4820dc0d7e12f4015/"
+					dirs_to_del.append(s)
+			i += 1
+		if len(dirs_to_del) > to_keep:
+			dirs_to_del = dirs_to_del[:-to_keep]
+			print("deleting old deploys: %s" % str(dirs_to_del))
+			for d in dirs_to_del:
+				run("rm -rf %s" % d)
+
 def deploy():
 	if not os.path.exists("secrets.json"): abort("secrets.json doesn't exist locally")
 	#git_pull()
 	git_ensure_clean()
 	local("./scripts/build.sh")
-	ensure_remote_dir_exists('www/app')
+	ensure_remote_dir_exists(app_dir)
 	ensure_remote_file_exists('www/data/sumatrapdf')
 	sha1 = git_trunk_sha1()
-	code_path_remote = 'www/app/' + sha1
+	code_path_remote = app_dir + '/' + sha1
 	if files.exists(code_path_remote):
 		abort('code for revision %s already exists on the server' % sha1)
 	zip_path = sha1 + ".zip"
 	zip_files(zip_path)
-	zip_path_remote = 'www/app/' + zip_path
+	zip_path_remote = app_dir + '/' + zip_path
 	put(zip_path, zip_path_remote)
 	delete_file(zip_path)
-	with cd('www/app'):
+	with cd(app_dir):
 		run('unzip -q -x %s -d %s' % (zip_path, sha1))
 		run('rm -f %s' % zip_path)
 	# make sure it can build
 	with cd(code_path_remote):
 		run("./scripts/build.sh")
 
-	curr_dir = 'www/app/current'
+	curr_dir = app_dir + '/current'
 	if files.exists(curr_dir):
 		# shut-down currently running instance
 		with cd(curr_dir):
 			run("/sbin/start-stop-daemon --stop --oknodo --exec fofou_app")
 		# rename old current as prev for easy rollback of bad deploy
-		with cd('www/app'):
+		with cd(app_dir):
 			run('rm -f prev')
 			run('mv current prev')
 
 	# make this version current
-	with cd('www/app'):
+	with cd(app_dir):
 		run("ln -s %s current" % sha1)
 
 	# start it
 	with cd(curr_dir):
 		run("/sbin/start-stop-daemon --start --background --chdir /home/fofou/www/app/current --exec fofou_app -- --log fofou_app.log --production")
 		run("ps aux | grep _app")
+
+    delete_old_deploys()
