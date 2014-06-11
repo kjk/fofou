64145836140f2a424d0a4b324f38969453a6893a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-06-11 10:41:06 -0700

    appengine version moved to https://github.com/kjk/fofou_appengine

--- appengine/offsets.py
+++
@@ -1,6 +0,0 @@
-# This code is in Public Domain. Take all the code you want, I'll just write more.
-
-(POST_ID, POST_MSG, POST_NAME, POST_EMAIL, POST_URL, POST_POSTED_ON, POST_POSTER_IP, POST_POSTER_KEY, POST_UNIQUE_KEY, POST_DELETED, POST_LAST_MOD) = range(11)
-(TOPIC_ID, TOPIC_FIRST_POST, TOPIC_SUBJECT) = range(3)
-(TP_TOPIC_ID, TP_POST_ID) = range(2)
-
