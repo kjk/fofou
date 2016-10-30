047385624e81ed1af184352281ba3b61dc3275fa
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-10-29 18:48:14 -0700

    update for latest kjk/u

--- s3backup.go
+++ s3backup.go
@@ -280,7 +280,7 @@ func doBackup(config *BackupConfig) {
 	if err != nil {
 		return
 	}
-	sha1, err := u.FileSha1(zipLocalPath)
+	sha1, err := u.Sha1HexOfFile(zipLocalPath)
 	if err != nil {
 		return
 	}
